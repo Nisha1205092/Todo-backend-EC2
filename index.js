259e83ab7593962b84292035963ab476ae881e84
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-08-02 17:39:18 +0600

    APIs rewritten

--- index.js
+++ index.js
@@ -3,76 +3,99 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const { fetchAllTodos, saveATodo, Todos } = require('./utils');
+const {
+    fetchAllTodos,
+    createUser,
+    verifyUser,
+    updateTodo,
+    Todo,
+    User,
+} = require('./utils');
 const path = require('path');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
-mongoose.connect('mongodb+srv://aminarahman01:N1sh4M0ng0@cluster0.utbunmm.mongodb.net/todo-app')
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
 
-//this endpoint works as a front-end
-// essentially this is a full-stack TODO app now!!
-app.get('/', (req, res) => {
-    return res.sendFile('index.html', { root: path.join(__dirname, 'public') });
+
+// user signs up
+// create a user document
+app.post('/signup', (req, res) => {
+    // create a user document in MongoDB
+    createUser(req, res);
 })
-app.get('/todos', async (req, res) => {
-    const todoArray = await fetchAllTodos();
-    if (todoArray.length === 0) {
-        return res.status(404).json({ message: 'empty' })
-    }
-    return res.json({ todoArray });
-});
 
-app.get('/todos/:id', async (req, res) => {
-    const id = req.params.id;
-    try {
-        const todo = await Todos.findById(id);
-        if (todo) {
-            return res.json({ todo })
+// user signs in
+// READ all the todos
+app.get('/user/signin', verifyUser, async (req, res) => {
+    console.log({ user: req.user })
+    if (req.user) {
+        console.log('number of todos', req.user.todoList.length)
+        const todos = await fetchAllTodos(req.user.todoList);
+        return res.json({
+            message: 'user signed in',
+            todos
+        })
+    }
+    return res.status(404).json({ message: 'emailNotFound' })
+    //fetchAllTodos(user)
+})
+
+// CREATE todo
+app.post('/user/todo', verifyUser, async (req, res) => {
+    console.log({ user: req.user })
+    if (req.user) {
+        // create a todo
+        const newTodo = new Todo(req.body)
+        await newTodo.save();
+        // add the todoId to the corresponding user's
+        // todoList array
+        req.user.todoList.push(newTodo.id)
+        await req.user.save()
+        return res.json({ message: 'Todo added successfully' })
+    }
+    return res.status(404).json({ message: 'emailNotFound' })
+})
+
+// UPDATE a todo
+app.put('/user/todo/:todoId', verifyUser, async (req, res) => {
+    // if user email is found
+    if (req.user) {
+        await updateTodo(req, res);
+    } else {
+        return res.status(404).json({ message: 'emailNotFound' })
+    }
+})
+
+// DELETE a todo
+app.delete('/user/todo/:todoId', verifyUser, async (req, res) => {
+    // if user email is found
+    if (req.user) {
+        console.log({ user: req.user })
+        try {
+            const id = req.params.todoId;
+            console.log({ id })
+            // delete the entry in the todos collection
+            await Todo.findByIdAndDelete(id)
+            // delete the id in the users collection as well
+            // Delete the todoId from the user.todoList array
+            const doc = await User.findByIdAndUpdate(
+                req.user._id,
+                { $pull: { todoList: id } },
+                { new: true } // findOneAndUpdate() will instead give you the object after update was applied.
+            )
+            // after update
+            console.log({ doc })
+            return res.send({ message: 'deleted successfully' })
+        } catch (error) {
+            return res.status(500).json({ error: 'Failed to delete todo.' });
         }
-        return res.status(404).json({ message: 'todo not found' })
-    } catch (error) {
-        return res.status(500).json({ message: 'internal error' })
-    }
-
-})
-
-app.post('/todos', async (req, res) => {
-    const title = req.body.title;
-    const completed = req.body.completed;
-    const description = req.body.description;
-
-    const id = await saveATodo({ title, completed, description });
-
-    res.status(201).json({ id })
-})
-
-app.put('/todos/:id', async (req, res) => {
-    const id = req.params.id;
-    /*
-    By default, when using findByIdAndUpdate(), 
-    the method returns the original document before the update was applied. 
-    However, by setting { new: true }, 
-    you instruct the method to return the modified document instead.
-    */
-    try {
-        const todo = await Todos.findByIdAndUpdate(id, req.body, { new: true });
-        if (todo) {
-            return res.json({ todo, message: 'todo updated successfully' })
-        }
-        return res.status(404).json({ message: 'todo not found' })
-    } catch (error) {
-        return res.status(500).json({ message: 'internal error' })
+    } else {
+        return res.status(404).json({ message: 'emailNotFound' })
     }
 })
 
-app.delete('/todos/:id', async (req, res) => {
-    const id = req.params.id;
-    await Todos.findByIdAndDelete(id)
-    return res.send();
-})
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
--- utils.js
+++ utils.js
@@ -1,31 +1,108 @@
 const mongoose = require('mongoose');
 
-const todosSchema = new mongoose.Schema({
-    id: Number,
-    title: String,
-    completed: Boolean,
-    description: String
-})
+// Define schema for user
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    uid: { type: String, required: true },
+    todoList: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Todo' }]
+});
 
-const Todos = mongoose.model('Todos', todosSchema)
+// Define schema for todo
+const todoSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: String,
+    completed: { type: Boolean, required: true },
+    important: { type: Boolean, default: false, required: true },
+    creationDate: { type: Date, default: Date.now, required: true },
+    updatedAt: { type: Date, default: Date.now },
+    deadlineDate: Date,
+    images: { type: [String], default: ['string1', 'string2'] },
+    tags: { type: [String], default: ['personal'] },
+});
 
-const fetchAllTodos = async () => {
-    const todos = await Todos.find();
-    console.log({ todos })
-    if (todos.length === 0) {
-        console.log('no todos')
+// Create models for user and todo
+const User = mongoose.model('User', userSchema);
+const Todo = mongoose.model('Todo', todoSchema);
+
+const fetchAllTodos = async (todoList) => {
+    const todos = await Promise.all(
+        todoList.map(async (item) => {
+            try {
+                const todo = await Todo.findById(item);
+                return todo;
+            } catch (err) {
+                console.log('todo not found:', item);
+                return null;
+            }
+        })
+    );
+
+    // Filter out any null values from todos array (failed findById calls)
+    return todos.filter((todo) => todo !== null);
+};
+
+const createUser = async (req, res) => {
+    const { email, uid } = req.body;
+    const username = email.split('@')[0].toUpperCase()
+    const user = await User.findOne({ email })
+
+    console.log({ user })
+    // this is improbable
+    // using firebase auth
+    // we handle this case in the frontend
+    if (user) {
+        return res.status(403).json({ message: 'User already exists' })
     }
-    return todos;
+    const newUser = new User({ username, email, uid })
+    newUser.save()
+    return res.json({ message: 'User created successfully' })
 }
 
-const saveATodo = async ({ title, completed, description }) => {
-    const newTodo = new Todos({ title, completed, description })
-    await newTodo.save()
-    return newTodo._id
+// middleware
+const verifyUser = async (req, res, next) => {
+    const email = req.headers.email;
+    console.log({ email })
+    const user = await findUser(email);
+    req.user = user;
+    next()
 }
+
+const updateTodo = async (req, res) => {
+    const todoId = req.params.todoId;
+    /*
+    By default, when using findByIdAndUpdate(), 
+    the method returns the original document before the update was applied. 
+    However, by setting { new: true }, 
+    we instruct the method to return the modified document instead.
+    */
+    try {
+        const todo = await Todo.findByIdAndUpdate(todoId, req.body, { new: true })
+        return res.json({ message: 'todo updated successfully' })
+    } catch (error) {
+        return res.status(404).json({ message: 'Todo not found' })
+    }
+}
+
+// const findUser = async (email) => {
+//     const user = await User.findOne({ email })
+//     if (user) {
+//         return user;
+//     }
+//     return null;
+// }
+
+// const saveATodo = async ({ title, completed, description }) => {
+//     const newTodo = new Todo({ title, completed, description })
+//     await newTodo.save()
+//     return newTodo._id
+// }
 
 module.exports = {
-    Todos,
+    Todo,
+    User,
     fetchAllTodos,
-    saveATodo,
+    createUser,
+    verifyUser,
+    updateTodo,
 }
