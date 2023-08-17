3b7d90de7b34a1f2e8f0f28eb2c6710e725309e4
Amina Rahman Nisha <aminarahman01@gmail.com>
2023-08-17 17:11:25 +0600

    created AMI for the lost key

--- CD-scripts/script-local.sh
+++ CD-scripts/script-local.sh
@@ -1,4 +1,4 @@
 # where the my-todo-app.pem is located in my local machine
 # before CI/CD era
-ssh -t -i my-todo-app.pem ubuntu@ec2-13-234-38-7.ap-south-1.compute.amazonaws.com 
+ssh -t -i my-todo-app.pem ubuntu@ec2-15-206-92-147.ap-south-1.compute.amazonaws.com 
 "sudo bash ~/deploy.sh"
